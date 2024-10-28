606f1fb076a20a4445bb7fc64cedbd6b1baa5b57
Shiv Bhonde <shivbhonde04@gmail.com>
2024-10-28 14:28:03 +0700

    add startapp

--- src/app.ts
+++ src/app.ts
@@ -79,7 +79,7 @@ bot.command("link", (ctx) =>
   ctx.reply(
     link(
       "Launch",
-      "https://t.me/megazu_commitment_tracker_bot/MegaCommitments/activity",
+      "https://t.me/megazu_commitment_tracker_bot/MegaCommitments?startapp=start&mode=compact",
     ),
   ),
 );
